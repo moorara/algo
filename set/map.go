08695b4765ef77f8b90a8c68bc7a166f69d22b10
Milad <moorara@users.noreply.github.com>
2024-01-17 14:10:02 -0500

    [set] Add missing comments (#175)

--- set/map.go
+++ set/map.go
@@ -2,9 +2,11 @@ package set
 
 import "github.com/moorara/algo/generic"
 
-// Mapper
+// Mapper is a function for converting a member of a set from one type to another type.
 type Mapper[T, U any] func(T) U
 
+// Map converts a set from one type to another type.
+// You need to provide a compare function for the new type.
 func (f Mapper[T, U]) Map(s Set[T], equal generic.EqualFunc[U]) Set[U] {
 	members := make([]U, 0)
 	for _, m := range s.Members() {
