17007d432e84b67ff2faa14e054944bee39e949e
Milad <moorara@users.noreply.github.com>
2025-01-04 04:25:32 -0500

    Improve binary heaps (#229)
    * Improve binary heap: use pointers
    * Fix Indexed Binary Heap bug

--- heap/indexed_binary.go
+++ heap/indexed_binary.go
@@ -12,19 +12,19 @@ type indexedBinary[K, V any] struct {
 	cmpKey CompareFunc[K]
 	eqVal  EqualFunc[V]
 
-	cap  int              // maximum number of items on heap
-	n    int              // current number of items on heap
-	heap []int            // binary heap of indices using 1-based indexing
-	pos  []int            // map of indices to positions on heap
-	kvs  []KeyValue[K, V] // map of indices to key-values
+	n    int               // current number of items on heap
+	heap []int             // binary heap of indices using 1-based indexing
+	pos  []int             // map of indices to positions on heap
+	kvs  []*KeyValue[K, V] // map of indices to key-values
 }
 
 // NewIndexedBinary creates a new indexed binary heap that can be used as a priority queue.
+//
 // An indexed heap (priority queue) associates an index with each key-value pair.
 // It allows changing the key (priority) of an index, deleting by index, and looking up by index.
 // The size of an indexed binary heap is fixed.
 //
-// cap is the maximum number of items on the heap (priority queue).
+// cap is the maximum number of items on the heap.
 // cmpKey is a function for comparing two keys.
 // eqVal is a function for checking the equality of two values.
 func NewIndexedBinary[K, V any](cap int, cmpKey CompareFunc[K], eqVal EqualFunc[V]) IndexedHeap[K, V] {
@@ -36,16 +36,15 @@ func NewIndexedBinary[K, V any](cap int, cmpKey CompareFunc[K], eqVal EqualFunc[
 	return &indexedBinary[K, V]{
 		cmpKey: cmpKey,
 		eqVal:  eqVal,
-		cap:    cap,
 		n:      0,
 		heap:   make([]int, cap+1),
 		pos:    pos,
-		kvs:    make([]KeyValue[K, V], cap),
+		kvs:    make([]*KeyValue[K, V], cap),
 	}
 }
 
 func (h *indexedBinary[K, V]) validateIndex(i int) {
-	if i < 0 || i >= h.cap {
+	if i < 0 || i >= len(h.kvs) {
 		panic("index is out of range")
 	}
 }
@@ -56,35 +55,32 @@ func (h *indexedBinary[K, V]) compare(a, b int) int {
 	return h.cmpKey(h.kvs[i].Key, h.kvs[j].Key)
 }
 
+// swap exchanges the elements at indices i and j in the heap array
+// and updates their corresponding positions in the pos map.
+func (h *indexedBinary[K, V]) swap(i, j int) {
+	h.heap[i], h.heap[j] = h.heap[j], h.heap[i]
+	h.pos[h.heap[i]], h.pos[h.heap[j]] = i, j
+}
+
 // Promotion operation in heap (a.k.a. swim).
-// Exchange k with its parent (k/2) until heap is restored.
+// Exchange child k with its parent k/2 until the heap is restored.
 func (h *indexedBinary[K, V]) promote(k int) {
-	for k > 1 && h.compare(k/2, k) > 0 {
-		h.heap[k], h.heap[k/2] = h.heap[k/2], h.heap[k]
-		h.pos[h.heap[k]] = k
-		h.pos[h.heap[k/2]] = k / 2
-		k /= 2
+	for ; k > 1 && h.compare(k/2, k) > 0; k /= 2 {
+		h.swap(k, k/2)
 	}
 }
 
 // Demotion operation in heap (a.k.a. sink).
-// Exchange k with its smallest/largest child (j) until heap is restored.
+// Exchange parent k with its smallest/largest child j until the heap is restored.
 func (h *indexedBinary[K, V]) demote(k int) {
-	for 2*k <= h.n {
-		j := 2 * k
-		if j < h.n && h.compare(j, j+1) > 0 {
+	for j := 2 * k; j <= h.n; k, j = j, 2*j {
+		if j < h.n && h.compare(j+1, j) < 0 {
 			j++
 		}
-
-		if h.compare(k, j) <= 0 {
+		if h.compare(k, j) < 0 {
 			break
 		}
-
-		h.heap[k], h.heap[j] = h.heap[j], h.heap[k]
-		h.pos[h.heap[k]] = k
-		h.pos[h.heap[j]] = j
-
-		k = j
+		h.swap(k, j)
 	}
 }
 
@@ -100,15 +96,15 @@ func (h *indexedBinary[K, V]) IsEmpty() bool {
 
 // Insert adds a new key-value pair to the heap with an associated index.
 func (h *indexedBinary[K, V]) Insert(i int, key K, val V) {
-	// ContainsIndex validates the index as well
+	// ContainsIndex validates the index too.
 	if h.ContainsIndex(i) {
-		panic("index already on heap")
+		panic(fmt.Sprintf("index %d is already on the heap", i))
 	}
 
 	h.n++
 	h.heap[h.n] = i
 	h.pos[i] = h.n
-	h.kvs[i] = KeyValue[K, V]{
+	h.kvs[i] = &KeyValue[K, V]{
 		Key: key,
 		Val: val,
 	}
@@ -118,9 +114,9 @@ func (h *indexedBinary[K, V]) Insert(i int, key K, val V) {
 
 // ChangeKey changes the key associated with an index.
 func (h *indexedBinary[K, V]) ChangeKey(i int, key K) {
-	// ContainsIndex validates the index as well
+	// ContainsIndex validates the index too.
 	if !h.ContainsIndex(i) {
-		panic("index is not on heap")
+		panic(fmt.Sprintf("index %d is not on the heap", i))
 	}
 
 	h.kvs[i].Key = key
@@ -131,68 +127,61 @@ func (h *indexedBinary[K, V]) ChangeKey(i int, key K) {
 // Delete removes the extremum (minimum or maximum) key with its value and index on the heap.
 // If the heap is empty, the second return value will be false.
 func (h *indexedBinary[K, V]) Delete() (int, K, V, bool) {
-	var zeroK K
-	var zeroV V
-
 	if h.n == 0 {
+		var zeroK K
+		var zeroV V
 		return -1, zeroK, zeroV, false
 	}
 
 	i := h.heap[1]
-	key, val := h.kvs[i].Key, h.kvs[i].Val
-
-	h.heap[1], h.heap[h.n] = h.heap[h.n], h.heap[1]
+	ext := h.kvs[i]
+	h.swap(1, h.n)
 	h.n--
+
 	h.demote(1)
 
-	// Delete index
+	// Delete index and remove stale reference to help with garbage collection.
 	h.pos[i] = -1
+	h.kvs[i] = nil
 
-	// Remove stale references to help with garbage collection
-	h.kvs[i].Key, h.kvs[i].Val = zeroK, zeroV
-
-	return i, key, val, true
+	return i, ext.Key, ext.Val, true
 }
 
 // DeleteIndex removes a key-value pair and its associated index from the heap.
 // If the index is not valid or not on the heap, the second return value will be false.
 func (h *indexedBinary[K, V]) DeleteIndex(i int) (K, V, bool) {
-	var zeroK K
-	var zeroV V
-
-	// ContainsIndex validates the index as well
+	// ContainsIndex validates the index too.
 	if !h.ContainsIndex(i) {
+		var zeroK K
+		var zeroV V
 		return zeroK, zeroV, false
 	}
 
 	k := h.pos[i]
-	key, val := h.kvs[i].Key, h.kvs[i].Val
-
-	h.heap[k], h.heap[h.n] = h.heap[h.n], h.heap[k]
+	kv := h.kvs[i]
+	h.swap(k, h.n)
 	h.n--
+
 	h.promote(k)
 	h.demote(k)
 
-	// Delete index
+	// Delete index and remove stale reference to help with garbage collection.
 	h.pos[i] = -1
+	h.kvs[i] = nil
 
-	// Remove stale references to help with garbage collection
-	h.kvs[i].Key, h.kvs[i].Val = zeroK, zeroV
-
-	return key, val, true
+	return kv.Key, kv.Val, true
 }
 
 // DeleteAll deletes all keys with their values and indices on the heap, leaving it empty.
 func (h *indexedBinary[K, V]) DeleteAll() {
-	pos := make([]int, h.cap)
-	for i := range pos {
-		pos[i] = -1
-	}
-
 	h.n = 0
-	h.heap = make([]int, h.cap+1)
-	h.pos = pos
-	h.kvs = make([]KeyValue[K, V], h.cap)
+	h.heap = make([]int, len(h.heap))
+	h.pos = make([]int, len(h.pos))
+	h.kvs = make([]*KeyValue[K, V], len(h.kvs))
+
+	for i := range h.pos {
+		h.pos[i] = -1
+	}
 }
 
 // Peek returns the extremum (minimum or maximum) key with its value and index on the heap without removing it.
@@ -211,7 +200,7 @@ func (h *indexedBinary[K, V]) Peek() (int, K, V, bool) {
 // PeekIndex returns a key-value pair on the heap by its associated index without removing it.
 // If the index is not valid or not on the heap, the second return value will be false.
 func (h *indexedBinary[K, V]) PeekIndex(i int) (K, V, bool) {
-	// ContainsIndex validates the index as well
+	// ContainsIndex validates the index too.
 	if !h.ContainsIndex(i) {
 		var zeroK K
 		var zeroV V
@@ -229,8 +218,8 @@ func (h *indexedBinary[K, V]) ContainsIndex(i int) bool {
 
 // ContainsKey returns true if a given key is on the heap.
 func (h *indexedBinary[K, V]) ContainsKey(key K) bool {
-	for k := 0; k < h.n; k++ {
-		if h.cmpKey(h.kvs[k].Key, key) == 0 {
+	for i := 0; i < h.n; i++ {
+		if h.kvs[i] != nil && h.cmpKey(h.kvs[i].Key, key) == 0 {
 			return true
 		}
 	}
@@ -240,8 +229,8 @@ func (h *indexedBinary[K, V]) ContainsKey(key K) bool {
 
 // ContainsValue returns true if a given value is on the heap.
 func (h *indexedBinary[K, V]) ContainsValue(val V) bool {
-	for k := 0; k < h.n; k++ {
-		if h.eqVal(h.kvs[k].Val, val) {
+	for i := 0; i < h.n; i++ {
+		if h.kvs[i] != nil && h.eqVal(h.kvs[i].Val, val) {
 			return true
 		}
 	}
