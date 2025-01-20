0652346b34963ff773ae7816ba1a6782b62146d9
Milad Irannejad <moorara@users.noreply.github.com>
2025-01-20 03:18:20 -0500

    Work on SLR parser

--- parser/slr/slr.go
+++ parser/slr/slr.go
@@ -1,60 +1,153 @@
 // Package slr provides data structures and algorithms for building Simple LR (SLR) parsers.
 // An SLR parser is a bottom-up parser for the class of LR(1) grammars.
 //
-// Bottom-up parsing constructs a parse tree for an input string
-// starting at the leaves (bottom) and working towards the root (top).
-// This process involves reducing a string w to the start symbol of the grammar.
-// At each reduction step, a specific substring matching the body of a production
-// is replaced by the non-terminal at the head of that production.
-//
-// Bottom-up parsing during a left-to-right scan of the inputconstructs a rightmost derivation in reverse:
-//
-//	S = γ₀ ⇒ᵣₘ γ₁ ⇒ᵣₘ γ₂ ⇒ᵣₘ ... ⇒ᵣₘ γₙ₋₁ ⇒ᵣₘ γₙ = w
-//
-// At each step, the handle βₙ in γₙ is replaced by the head of the production Aₙ → βₙ
-// to obtain the previous right-sentential form γₙ₋₁.
-// If the process produces the start symbol S as the only sentential form, parsing is complete.
-// If a grammar is unambiguous, then every right-sentential form of the grammar has exactly one handle.
-//
-// The most common type of bottom-up parser is LR(k) parsing.
-// The L is for left-to-right scanning of the input, the R for constructing a rightmost derivation in reverse,
-// and the k for the number of input symbols of lookahead that are used in making parsing decisions.
-//
-// Advantages of LR parsing:
-//
-//   - Can recognize nearly all programming language constructs defined by context-free grammars.
-//   - Detects syntax errors at the earliest possible point during a left-to-right scan.
-//   - The class of grammars that can be parsed using LR methods is a proper superset of
-//     the class of grammars that can be parsed with predictive or LL methods.
-//     For a grammar to be LR(k), we must be able to recognize the occurrence of the right side of
-//     a production in a right-sentential form, with k input symbols of lookahead.
-//     This requirement is far less stringent than that for LL(k) grammars where we must be able
-//     to recognize the use of a production seeing only the first k symbols of what its right side derives.
-//
-// In LR(k) parsing, the cases k = 0 or k = 1 are most commonly used in practical applications.
-// LR parsing methods use pushdown automata (PDA) to parse an input string.
-// A pushdown automaton is a type of automaton used for Type 2 languages (context-free languages) in the Chomsky hierarchy.
-// A PDA uses a state machine with a stack.
-// The next state is determined by the current state, the next input, and the top of the stack.
-// LR(0) parsers do not rely on any lookahead to make parsing decisions.
-// An LR(0) parser bases its decisions entirely on the current state and the parsing stack.
-//
-// LR(1) parsers determine the next state based on the current state, one lookahead symbol, and the top of the stack.
 // An SLR parser uses the canonical LR(0) items to construct the state machine (DFA).
 // An SLR parser is less powerful than a canonical LR(1) parser.
 // SLR simplifies the construction process but sacrifices some parsing power compared to canonical LR(1).
 //
-// Shift-reduce parsing is a bottom-up parsing technique that uses
-// a stack for grammar symbols and an input buffer for the remaining string.
-// The parser alternates between shifting symbols from the input to the stack
-// and reducing the top of the stack based on grammar rules.
-// This process continues until the stack contains only the start symbol and the input is empty, or an error occurs.
-//
-// Certain context-free grammars cannot be parsed using shift-reduce parsers
-// because they may encounter shift/reduce conflicts (indecision between shifting or reducing)
-// or reduce/reduce conflicts (indecision between multiple reductions).
-// Technically speaking, these grammars are not in the LR(k) class.
-//
 // For more details on parsing theory,
 // refer to "Compilers: Principles, Techniques, and Tools (2nd Edition)".
 package slr
+
+import (
+	"github.com/moorara/algo/grammar"
+	"github.com/moorara/algo/lexer"
+	"github.com/moorara/algo/parser"
+	"github.com/moorara/algo/parser/lr"
+)
+
+// slrParser is an SLR parser for LR(0) grammars.
+// It implements the parser.Parser interface.
+type slrParser struct {
+	G     grammar.CFG
+	lexer lexer.Lexer
+}
+
+// New creates a new SLR parser for a given context-free grammar (CFG).
+// It requires a lexer for lexical analysis, which reads the input tokens (terminal symbols).
+func New(G grammar.CFG, lexer lexer.Lexer) parser.Parser {
+	return &slrParser{
+		G:     G,
+		lexer: lexer,
+	}
+}
+
+// Parse analyzes input tokens (terminal symbols) provided by the lexical analyzer
+// and attempts to construct a syntactic representation (i.e., a parse tree) of the input.
+//
+// The Parse method invokes the given function for each production and token during parsing.
+// It returns an error if the input fails to conform to the grammar rules.
+func (p *slrParser) Parse(yield parser.Action) error {
+
+	/*
+	 * INPUT:  • A lexer for reading string w.
+	 *         • A parsing table M for grammar G.
+	 * OUTPUT: • If w ∈ L(G), a leftmost derivation of w; otherwise an error indication.
+	 *
+	 * METHOD: TBD
+	 */
+
+	return nil
+}
+
+// BuildParsingTable constructs a parsing table for an SLR parser.
+//
+// This method constructs an LR(0) parsing table for any context-free grammar.
+// To identify errors in the table, use the Error method.
+func BuildParsingTable(G grammar.CFG) *lr.ParsingTable {
+	/*
+	 * INPUT:  An augmented grammar G′.
+	 * OUTPUT: The SLR-parsing table functions ACTION and GOTO for G′.
+	 */
+
+	augG := lr.Augment(G)
+	first := augG.ComputeFIRST()
+	follow := augG.ComputeFOLLOW(first)
+
+	init := Initial(augG)
+	CLOSURE := LR0Closure(augG)
+
+	// 1. Construct C = {I₀, I₁, ..., Iₙ}, the collection of sets of LR(0) items for G′.
+	C := lr.Canonical(augG, init, CLOSURE)
+
+	states := lr.BuildStateMap(C)
+	terminals := G.OrderTerminals()
+	_, _, nonTerminals := G.OrderNonTerminals()
+
+	table := lr.NewParsingTable(states.All(), terminals, nonTerminals)
+
+	// 2. State i is constructed from I.
+	for i, I := range states {
+		// The parsing actions for state i are determined as follows:
+
+		for item := range I.All() {
+			if item, ok := item.(LR0Item); ok {
+				// If A → α•aβ is in Iᵢ and GOTO(Iᵢ,a) = Iⱼ (a must be a terminal)
+				if X, ok := item.DotSymbol(); ok {
+					if a, ok := X.(grammar.Terminal); ok {
+						J := lr.GOTO(CLOSURE, I, a)
+						j := states.For(J)
+
+						// Set ACTION[i,a] to SHIFT j
+						table.AddACTION(lr.State(i), a, lr.Action{
+							Type:  lr.SHIFT,
+							State: j,
+						})
+					}
+				}
+
+				// If A → α• is in Iᵢ (A may not be S′)
+				if item.IsComplete() && !item.IsFinal() {
+					followA := follow(item.Head)
+
+					// For all a in FOLLOW(A)
+					for a := range followA.Terminals.All() {
+						// Set ACTION[i,a] to REDUCE A → α
+						table.AddACTION(lr.State(i), a, lr.Action{
+							Type:       lr.REDUCE,
+							Production: item.Production,
+						})
+					}
+
+					if followA.IncludesEndmarker {
+						// Set ACTION[i,$] to REDUCE A → α
+						table.AddACTION(lr.State(i), grammar.Endmarker, lr.Action{
+							Type:       lr.REDUCE,
+							Production: item.Production,
+						})
+					}
+				}
+
+				// If S′ → S• is in Iᵢ
+				if item.IsFinal() {
+					// Set ACTION[i,$] to ACCEPT
+					table.AddACTION(lr.State(i), grammar.Endmarker, lr.Action{
+						Type: lr.ACCEPT,
+					})
+				}
+
+				// If any conflicting actions result from the above rules, the grammar is not SLR(1).
+				// The table.Error() method will list all conflicts, if any exist.
+			}
+		}
+
+		// 3. The goto transitions for state i are constructed for all non-terminals A using the rule:
+		// If GOTO(Iᵢ,A) = Iⱼ
+		for A := range augG.NonTerminals.All() {
+			if !A.Equals(augG.Start) {
+				J := lr.GOTO(CLOSURE, I, A)
+				j := states.For(J)
+
+				// Set GOTO[i,A] = j
+				table.SetGOTO(lr.State(i), A, j)
+			}
+		}
+
+		// 4. All entries not defined by rules (2) and (3) are made ERROR.
+	}
+
+	// 5. The initial state of the parser is the one constructed from the set of items containing "S′ → •S".
+	// The BuildStateMap function ensures that state 0 always includes the initial item "S′ → •S".
+
+	return table
+}
--- parser/slr/slr_test.go
+++ parser/slr/slr_test.go
@@ -1,6 +1,17 @@
 package slr
 
-import "github.com/moorara/algo/grammar"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/moorara/algo/grammar"
+	"github.com/moorara/algo/parser/lr"
+)
+
+var starts = []grammar.NonTerminal{
+	"E′",
+}
 
 var prods = [][]grammar.Production{
 	{
@@ -12,44 +23,101 @@ var prods = [][]grammar.Production{
 		{Head: "F", Body: grammar.String[grammar.Symbol]{grammar.Terminal("("), grammar.NonTerminal("E"), grammar.Terminal(")")}},    // F → ( E )
 		{Head: "F", Body: grammar.String[grammar.Symbol]{grammar.Terminal("id")}},                                                    // F → id
 	},
-	{
-		{Head: "grammar′", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("grammar")}},                           // grammar′ → grammar
-		{Head: "grammar", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("name"), grammar.NonTerminal("decls")}}, // grammar → name decls
-		{Head: "name", Body: grammar.String[grammar.Symbol]{grammar.Terminal("GRAMMAR"), grammar.Terminal("IDENT")}},       // name → GRAMMAR IDENT
-		{Head: "decls", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("decls"), grammar.NonTerminal("decl")}},   // decls → decls decl
-		{Head: "decls", Body: grammar.E}, // decls → ε
-		{Head: "decl", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("token")}},                                                  // decl → token
-		{Head: "decl", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rule")}},                                                   // decl → rule
-		{Head: "token", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN"), grammar.Terminal("="), grammar.Terminal("STRING")}}, // token → TOKEN "=" STRING
-		{Head: "token", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN"), grammar.Terminal("="), grammar.Terminal("REGEX")}},  // token → TOKEN "=" REGEX
-		{Head: "rule", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("lhs"), grammar.Terminal("="), grammar.NonTerminal("rhs")}}, // rule → lhs "=" rhs
-		{Head: "rule", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("lhs"), grammar.Terminal("=")}},                             // rule → lhs "="
-		{Head: "lhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("nonterm")}},                                                 // lhs → nonterm
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rhs"), grammar.NonTerminal("rhs")}},                         // rhs → rhs rhs
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rhs"), grammar.Terminal("|"), grammar.NonTerminal("rhs")}},  // rhs → rhs "|" rhs
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("("), grammar.NonTerminal("rhs"), grammar.Terminal(")")}},       // rhs → "(" rhs ")"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("["), grammar.NonTerminal("rhs"), grammar.Terminal("]")}},       // rhs → "[" rhs "]"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("{"), grammar.NonTerminal("rhs"), grammar.Terminal("}")}},       // rhs → "{" rhs "}"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("{{"), grammar.NonTerminal("rhs"), grammar.Terminal("}}")}},     // rhs → "{{" rhs "}}"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("nonterm")}},                                                 // rhs → nonterm
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("term")}},                                                    // rhs → term
-		{Head: "nonterm", Body: grammar.String[grammar.Symbol]{grammar.Terminal("IDENT")}},                                                  // nonterm → IDENT
-		{Head: "term", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN")}},                                                     // term → TOKEN
-		{Head: "term", Body: grammar.String[grammar.Symbol]{grammar.Terminal("STRING")}},                                                    // term → STRING
-	},
 }
 
 var grammars = []grammar.CFG{
 	grammar.NewCFG(
-		[]grammar.Terminal{"+", "-", "*", "/", "(", ")", "id"},
+		[]grammar.Terminal{"+", "*", "(", ")", "id"},
 		[]grammar.NonTerminal{"E", "T", "F"},
 		prods[0][1:],
 		"E",
 	),
-	grammar.NewCFG(
+}
+
+func getTestParsingTables() []*lr.ParsingTable {
+	pt0 := lr.NewParsingTable(
+		[]lr.State{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+		[]grammar.Terminal{"(", ")", "*", "+", "id"},
+		[]grammar.NonTerminal{"E", "T", "F"},
+	)
+
+	pt0.AddACTION(0, "(", lr.Action{Type: lr.SHIFT, State: 9})
+	pt0.AddACTION(0, "id", lr.Action{Type: lr.SHIFT, State: 10})
+	pt0.AddACTION(1, "+", lr.Action{Type: lr.SHIFT, State: 5})
+	pt0.AddACTION(1, grammar.Endmarker, lr.Action{Type: lr.ACCEPT})
+	pt0.AddACTION(2, ")", lr.Action{Type: lr.REDUCE, Production: &prods[0][1]})
+	pt0.AddACTION(2, "*", lr.Action{Type: lr.SHIFT, State: 7})
+	pt0.AddACTION(2, "+", lr.Action{Type: lr.REDUCE, Production: &prods[0][1]})
+	pt0.AddACTION(2, grammar.Endmarker, lr.Action{Type: lr.REDUCE, Production: &prods[0][1]})
+	pt0.AddACTION(3, ")", lr.Action{Type: lr.REDUCE, Production: &prods[0][5]})
+	pt0.AddACTION(3, "*", lr.Action{Type: lr.REDUCE, Production: &prods[0][5]})
+	pt0.AddACTION(3, "+", lr.Action{Type: lr.REDUCE, Production: &prods[0][5]})
+	pt0.AddACTION(3, grammar.Endmarker, lr.Action{Type: lr.REDUCE, Production: &prods[0][5]})
+	pt0.AddACTION(4, ")", lr.Action{Type: lr.REDUCE, Production: &prods[0][3]})
+	pt0.AddACTION(4, "*", lr.Action{Type: lr.REDUCE, Production: &prods[0][3]})
+	pt0.AddACTION(4, "+", lr.Action{Type: lr.REDUCE, Production: &prods[0][3]})
+	pt0.AddACTION(4, grammar.Endmarker, lr.Action{Type: lr.REDUCE, Production: &prods[0][3]})
+	pt0.AddACTION(5, "(", lr.Action{Type: lr.SHIFT, State: 9})
+	pt0.AddACTION(5, "id", lr.Action{Type: lr.SHIFT, State: 10})
+	pt0.AddACTION(6, ")", lr.Action{Type: lr.SHIFT, State: 3})
+	pt0.AddACTION(6, "+", lr.Action{Type: lr.SHIFT, State: 5})
+	pt0.AddACTION(7, "(", lr.Action{Type: lr.SHIFT, State: 9})
+	pt0.AddACTION(7, "id", lr.Action{Type: lr.SHIFT, State: 10})
+	pt0.AddACTION(8, ")", lr.Action{Type: lr.REDUCE, Production: &prods[0][2]})
+	pt0.AddACTION(8, "*", lr.Action{Type: lr.SHIFT, State: 7})
+	pt0.AddACTION(8, "+", lr.Action{Type: lr.REDUCE, Production: &prods[0][2]})
+	pt0.AddACTION(8, grammar.Endmarker, lr.Action{Type: lr.REDUCE, Production: &prods[0][2]})
+	pt0.AddACTION(9, "(", lr.Action{Type: lr.SHIFT, State: 9})
+	pt0.AddACTION(9, "id", lr.Action{Type: lr.SHIFT, State: 10})
+	pt0.AddACTION(10, ")", lr.Action{Type: lr.REDUCE, Production: &prods[0][6]})
+	pt0.AddACTION(10, "*", lr.Action{Type: lr.REDUCE, Production: &prods[0][6]})
+	pt0.AddACTION(10, "+", lr.Action{Type: lr.REDUCE, Production: &prods[0][6]})
+	pt0.AddACTION(10, grammar.Endmarker, lr.Action{Type: lr.REDUCE, Production: &prods[0][6]})
+	pt0.AddACTION(11, ")", lr.Action{Type: lr.REDUCE, Production: &prods[0][4]})
+	pt0.AddACTION(11, "*", lr.Action{Type: lr.REDUCE, Production: &prods[0][4]})
+	pt0.AddACTION(11, "+", lr.Action{Type: lr.REDUCE, Production: &prods[0][4]})
+	pt0.AddACTION(11, grammar.Endmarker, lr.Action{Type: lr.REDUCE, Production: &prods[0][4]})
+
+	pt0.SetGOTO(0, "E", 1)
+	pt0.SetGOTO(0, "T", 8)
+	pt0.SetGOTO(0, "F", 11)
+	pt0.SetGOTO(5, "T", 2)
+	pt0.SetGOTO(5, "F", 11)
+	pt0.SetGOTO(7, "F", 4)
+	pt0.SetGOTO(9, "E", 6)
+	pt0.SetGOTO(9, "T", 8)
+	pt0.SetGOTO(9, "F", 11)
+
+	pt1 := lr.NewParsingTable(
+		[]lr.State{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36},
 		[]grammar.Terminal{"=", "|", "(", ")", "[", "]", "{", "}", "{{", "}}", "GRAMMAR", "IDENT", "TOKEN", "STRING", "REGEX"},
 		[]grammar.NonTerminal{"grammar", "name", "decls", "decl", "token", "rule", "lhs", "rhs", "nonterm", "term"},
-		prods[1][1:],
-		"grammar",
-	),
+	)
+
+	return []*lr.ParsingTable{pt0, pt1}
+}
+
+func TestBuildParsingTable(t *testing.T) {
+	pt := getTestParsingTables()
+
+	tests := []struct {
+		name          string
+		G             grammar.CFG
+		expectedTable *lr.ParsingTable
+	}{
+		{
+			name:          "OK",
+			G:             grammars[0],
+			expectedTable: pt[0],
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.NoError(t, tc.G.Verify())
+			table := BuildParsingTable(tc.G)
+
+			assert.True(t, table.Equals(tc.expectedTable))
+		})
+	}
 }
