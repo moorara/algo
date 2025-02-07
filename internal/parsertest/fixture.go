1918e1c646a872d2feedb15b142348f5c273466f
Milad Irannejad <moorara@users.noreply.github.com>
2025-02-06 23:17:39 -0500

    Remove EBNF grammar from fixtures

--- internal/parsertest/fixture.go
+++ internal/parsertest/fixture.go
@@ -43,44 +43,6 @@ var Prods = [][]*grammar.Production{
 		{Head: "E", Body: grammar.String[grammar.Symbol]{grammar.Terminal("("), grammar.NonTerminal("E"), grammar.Terminal(")")}},    // E → ( E )
 		{Head: "E", Body: grammar.String[grammar.Symbol]{grammar.Terminal("id")}},                                                    // E → id
 	},
-	{ // G5
-		{Head: "grammar′", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("grammar")}},                                                      // grammar′ → grammar
-		{Head: "grammar", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("name"), grammar.NonTerminal("decls")}},                            // grammar → name decls
-		{Head: "name", Body: grammar.String[grammar.Symbol]{grammar.Terminal("grammar"), grammar.Terminal("IDENT"), grammar.NonTerminal("semi_opt")}}, // name → "grammar" IDENT semi_opt
-		{Head: "semi_opt", Body: grammar.String[grammar.Symbol]{grammar.Terminal(";")}},                                                               // semi_opt → ";"
-		{Head: "semi_opt", Body: grammar.E}, // semi_opt → ε
-		{Head: "decls", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("decls"), grammar.NonTerminal("decl")}}, // decls → decls decl
-		{Head: "decls", Body: grammar.E}, // decls → ε
-		{Head: "decl", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("token")}},                                                                                   // decl → token
-		{Head: "decl", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("directive")}},                                                                               // decl → directive
-		{Head: "decl", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rule")}},                                                                                    // decl → rule
-		{Head: "token", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN"), grammar.Terminal("="), grammar.Terminal("STRING"), grammar.NonTerminal("semi_opt")}}, // token → TOKEN "=" STRING semi_opt
-		{Head: "token", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN"), grammar.Terminal("="), grammar.Terminal("REGEX"), grammar.NonTerminal("semi_opt")}},  // token → TOKEN "=" REGEX semi_opt
-		{Head: "token", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN"), grammar.Terminal("="), grammar.Terminal("PREDEF"), grammar.NonTerminal("semi_opt")}}, // token → TOKEN "=" PREDEF semi_opt
-		{Head: "directive", Body: grammar.String[grammar.Symbol]{grammar.Terminal("@left"), grammar.NonTerminal("handles"), grammar.NonTerminal("semi_opt")}},                // directive → "@left" handles semi_opt
-		{Head: "directive", Body: grammar.String[grammar.Symbol]{grammar.Terminal("@right"), grammar.NonTerminal("handles"), grammar.NonTerminal("semi_opt")}},               // directive → "@right" handles semi_opt
-		{Head: "directive", Body: grammar.String[grammar.Symbol]{grammar.Terminal("@none"), grammar.NonTerminal("handles"), grammar.NonTerminal("semi_opt")}},                // directive → "@none" handles semi_opt
-		{Head: "handles", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("handles"), grammar.NonTerminal("term")}},                                                 // handles → handles term
-		{Head: "handles", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("handles"), grammar.NonTerminal("rule_handle")}},                                          // handles → handles rule_handle
-		{Head: "handles", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("term")}},                                                                                 // handles → term
-		{Head: "handles", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rule_handle")}},                                                                          // handles → rule_handle
-		{Head: "rule_handle", Body: grammar.String[grammar.Symbol]{grammar.Terminal("<"), grammar.NonTerminal("rule"), grammar.Terminal(">")}},                               // rule_handle → "<" rule ">"
-		{Head: "rule", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("lhs"), grammar.Terminal("="), grammar.NonTerminal("rhs"), grammar.Terminal(";")}},           // rule → lhs "=" rhs ";"
-		{Head: "rule", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("lhs"), grammar.Terminal("="), grammar.Terminal(";")}},                                       // rule → lhs "=" ";"
-		{Head: "lhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("nonterm")}},                                                                                  // lhs → nonterm
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rhs"), grammar.NonTerminal("rhs")}},                                                          // rhs → rhs rhs
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("("), grammar.NonTerminal("rhs"), grammar.Terminal(")")}},                                        // rhs → "(" rhs ")"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("["), grammar.NonTerminal("rhs"), grammar.Terminal("]")}},                                        // rhs → "[" rhs "]"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("{"), grammar.NonTerminal("rhs"), grammar.Terminal("}")}},                                        // rhs → "{" rhs "}"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.Terminal("{{"), grammar.NonTerminal("rhs"), grammar.Terminal("}}")}},                                      // rhs → "{{" rhs "}}"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rhs"), grammar.Terminal("|"), grammar.NonTerminal("rhs")}},                                   // rhs → rhs "|" rhs
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("rhs"), grammar.Terminal("|")}},                                                               // rhs → rhs "|"
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("nonterm")}},                                                                                  // rhs → nonterm
-		{Head: "rhs", Body: grammar.String[grammar.Symbol]{grammar.NonTerminal("term")}},                                                                                     // rhs → term
-		{Head: "nonterm", Body: grammar.String[grammar.Symbol]{grammar.Terminal("IDENT")}},                                                                                   // nonterm → IDENT
-		{Head: "term", Body: grammar.String[grammar.Symbol]{grammar.Terminal("TOKEN")}},                                                                                      // term → TOKEN
-		{Head: "term", Body: grammar.String[grammar.Symbol]{grammar.Terminal("STRING")}},                                                                                     // term → STRING
-	},
 }
 
 var Grammars = []*grammar.CFG{
@@ -118,19 +80,5 @@ var Grammars = []*grammar.CFG{
 		[]grammar.NonTerminal{"E"},
 		Prods[4][1:],
 		"E",
-	),
-	// G5
-	grammar.NewCFG(
-		[]grammar.Terminal{
-			"=", ";", "|", "(", ")", "[", "]", "{", "}", "{{", "}}", "<", ">",
-			"grammar", "@left", "@right", "@none",
-			"IDENT", "TOKEN", "STRING", "REGEX", "PREDEF",
-		},
-		[]grammar.NonTerminal{
-			"grammar", "name", "decls", "decl", "semi_opt", "token",
-			"directive", "handles", "rule_handle", "rule", "lhs", "rhs", "nonterm", "term",
-		},
-		Prods[5][1:],
-		"grammar",
 	),
 }
