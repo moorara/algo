6ec5dbf2ba33beb162b3c5976962e9baf827222e
Milad Irannejad <moorara@users.noreply.github.com>
2025-10-24 03:06:48 -0400

    Refactoring the interval package

--- spatial/spatial.go
+++ spatial/spatial.go
@@ -1,5 +1,17 @@
-// Package spatial implements spatial data structures.
+// Package spatial implements common spatial data structures.
 //
-// Spatial data structures are specialized data structures used to efficiently store, organize, and query
-// data that represents objects in space, such as points, lines, polygons, or volumes.
+// Spatial data structures efficiently store, organize, and query objectsin space—such as points, lines, polygons, or volumes.
+// These structures are essential for geometric and multi-dimensional data analysis, enabling fast spatial queries and operations.
+//
+// Homogeneous Dimensions (Geometric Data):
+//
+// In geometric spaces, all dimensions typically share the same unit and type (e.g., coordinates in meters).
+// Most spatial algorithms—like distance calculations and nearest neighbor searches assume homogeneous dimensions,
+// which simplifies computation and comparison.
+//
+// Heterogeneous Dimensions (Multi-Dimensional Data):
+//
+// Spatial structures can also index data with mixed units or types across dimensions (e.g., latitude, longitude, and temperature).
+// For heterogeneous data, it is crucial to normalize or transform dimensions to a common scale, or use distance metrics
+// that account for differences, to ensure meaningful spatial operations and comparisons.
 package spatial
