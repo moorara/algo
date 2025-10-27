9c16ef865bcafc809e20a1b3851b9987a9f2bdeb
Milad <moorara@users.noreply.github.com>
2025-10-27 00:12:31 -0400

    Fix range/disc (#332)

--- range/disc/disc_list.go
+++ range/disc/disc_list.go
@@ -80,7 +80,7 @@ func NewRangeList[T Discrete](opts *RangeListOpts[T], rs ...Range[T]) RangeList[
 
 	// Sort ranges by their low bound ascending.
 	sort.Quick(l.ranges, func(lhs, rhs Range[T]) int {
-		return int(lhs.Lo - rhs.Lo)
+		return int(lhs.Lo) - int(rhs.Lo)
 	})
 
 	// Merge overlapping and adjacent ranges.
